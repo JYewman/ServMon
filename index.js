4c7e4548306edb2342dd51966c144f4172c10d5a
JYewman <joshua.yewman@outlook.com>
2021-04-23 15:45:52 +0100

    Initial node project commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const launcher = require("launcher/launch.js")
+
+console.log("===========================")
+console.log("Welcome to ServMon!")
+console.log("Version 1.0A")
+console.log("===========================")
+
+launcher.launch()
